7da6e7954f0315fcaecbd6a1ccaa852829bac4e1
Samantha brake <sammy.brake@gmail.com>
2019-05-21 14:16:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -41,10 +41,10 @@ class Route {
 
   estimatedTime(peakHours) {
     if(peakHours) {
-      return this.blocksTravelled()/2; 
+      return this.blocksTravelled()/2;
     } else {
       return this.blocksTravelled()/3;
     };
-    
+
   }
 }
