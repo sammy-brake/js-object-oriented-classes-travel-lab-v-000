38b6226bfb4ea0456d36eea1bc306d900f8b826e
Samantha brake <sammy.brake@gmail.com>
2019-05-21 14:17:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
+let eastWest = [
+  '1st Avenue',
+  '2nd Avenue',
+  '3rd Avenue',
+  'Lexington Avenue',
+  'Park',
+  'Madison Avenue',
+  '5th Avenue'
+];
 
+
+
+
+class Driver {
+  constructor(name, startDate){
+    this.name = name;
+    this.startDate = new Date(startDate);
+  };
+
+  yearsExperienceFromBeginningOf(year) {
+    let dateVariable = new Date(year, 0);
+    let variableYear = dateVariable.getFullYear();
+    let startYear = this.startDate.getFullYear();
+    return variableYear - startYear;
+  };
+};
+
+class Route {
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+
+  blocksTravelled() {
+    let verticalBlocks = this.beginningLocation.vertical - this.endingLocation.vertical;
+    let horizontalStart = eastWest.indexOf(this.beginningLocation.horizontal);
+    let   horizontalEnd = eastWest.indexOf(this.endingLocation.horizontal);
+    let   horizontalBlocks = horizontalStart - horizontalEnd;
+    return -(verticalBlocks + horizontalBlocks);
+  }
+
+  estimatedTime(peakHours) {
+    if(peakHours) {
+      return this.blocksTravelled()/2;
+    } else {
+      return this.blocksTravelled()/3;
+    };
+
+  }
+}
