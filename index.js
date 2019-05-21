9b8e1747f489afae7a085fd4cd292d9e186db892
Samantha brake <sammy.brake@gmail.com>
2019-05-21 13:34:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,10 +5,7 @@ class Driver {
     this.startDate = new date(startDate);
   };
 
-  startDate() {
-    return new date(startDate);
-  }
-}
+  
 
 class Route {
   constructor(beginningLocation, endingLocation) {
